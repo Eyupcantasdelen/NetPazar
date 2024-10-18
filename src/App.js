20d16459d6dac5f90d7e87a5d35ea8ecb6e5b0de
Eyupcantasdelen <166173779+Eyupcantasdelen@users.noreply.github.com>
2024-10-18 22:35:04 +0300

    created initial pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import { Footer, Navbar } from "./components";
-import {Outlet,Navigate,Route,Routes,useLocations} from "react-router-dom"
+import {Outlet,Navigate,Route,Routes,useLocation} from "react-router-dom"
+import {About,Auth,Companies,CompanyProfile,FindProducts,ProductDetail,UploadProduct,UserProfile} from "./pages"
+function Layout (){
+     const user=true;
+     const location=useLocation();
+    return user ? (<Outlet/>):(<Navigate to="user-auth" state={{from:location}} replace/>)
+}
+
 function App() {
+  const user={};
   return (
     <main>
-      <Navbar></Navbar>
-      <Footer></Footer>
+      <Navbar/>
+      <Routes>
+          <Route element={<Layout/>}>
+              <Route path="/" element={<Navigate to="/find-products" replace={true}/>}/>
+              <Route path="/find-products" element={<FindProducts/>}/>
+              <Route path="/companies" element={<Companies/>}/>
+              <Route path={user?.user?.accountType==="seeker" ?"/user-profile":"/user-profile:id"} element={<UserProfile/>}/>
+              
+              
+              <Route path={"/company-profile"} element={<CompanyProfile/>}/>
+              <Route path={"/company-profile/:id"} element={<CompanyProfile/>}/>  
+              <Route path={"/upload-product"} element={<UploadProduct/>}/>
+              <Route path={"/product-detail/:id"} element={<ProductDetail/>}/>
+          </Route>
+          <Route path="about-us" element={<About/>}/>
+          <Route path="user-auth" element={<Auth/>}/>
+
+      </Routes>
+      {user && <Footer/>}
     </main> 
   
   );
---
+++ src/pages/index.js
@@ -0,0 +1,10 @@
+import About from "./About"
+import Auth from "./Auth"
+import Companies from "./Companies"
+import CompanyProfile from "./CompanyProfile"
+import FindProducts from "./FindProducts"
+import ProductDetail from "./ProductDetail"
+import UploadProduct from "./UploadProduct"
+import UserProfile from "./UserProfile"
+
+export {About,Auth,Companies,CompanyProfile,FindProducts,ProductDetail,UploadProduct,UserProfile};

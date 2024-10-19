dfac5828f5f83d126052b321aafbae63cf1a2360
Eyupcantasdelen <166173779+Eyupcantasdelen@users.noreply.github.com>
2024-10-19 22:18:06 +0300

    making navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,13 @@
-import React from 'react'
+import React,{Fragment,useState} from 'react'
+import {Link} from "react-router-dom"
+import { Menu, Transition } from "@headlessui/react";
 
 const Navbar = () => {
+    const user={}
+    const [isOpen,setIsOpen]=useState(false);
+    const handleCloseNavbar=()=>{
+      setIsOpen((prev)=>!prev);
+    };
   return (
     <div>Navbar</div>
   )
